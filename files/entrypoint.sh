2891217fb2312166beff46260d2e2ddccd01ff81
cclloyd <cclloyd@cclloyd.com>
2021-10-21 23:31:57 -0400

    updated ci

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -10,25 +10,26 @@ cleanup ()
 trap cleanup SIGINT SIGTERM SIGQUIT
 
 while true; do
+  sleep 2
     # Check if NO_CLIENT is not set
-    if [[ -z "${NO_CLIENT}" ]]; then
-        # Run bot normally
-        if ! pg_isready -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -t 2
-        then
-            echo "Cannot connect to database... retrying in 5 seconds"
-            sleep 5 & wait $!
-        else
-            echo 'Starting app...'
-            cd /app || exit;
-            python api/manage.py migrate || exit;
-            nginx;
-            #python -m discordbot & wait $!
-            gunicorn -b 0.0.0.0
-        fi;
-    else
-        # Sleep indefinitely
-        sleep 15 & wait $!
-    fi;
+    #if [[ -z "${NO_CLIENT}" ]]; then
+    #    # Run bot normally
+    #    if ! pg_isready -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -t 2
+    #    then
+    #        echo "Cannot connect to database... retrying in 5 seconds"
+    #        sleep 5 & wait $!
+    #    else
+    #        echo 'Starting app...'
+    #        cd /app || exit;
+    #        python api/manage.py migrate || exit;
+    #        nginx;
+    #        #python -m discordbot & wait $!
+    #        gunicorn -b 0.0.0.0
+    #    fi;
+    #else
+    #    # Sleep indefinitely
+    #    sleep 15 & wait $!
+    #fi;
 done
 
 
--- test.Dockerfile
+++ test.Dockerfile
@@ -19,3 +19,5 @@ RUN set -ex \
     && apt-get update && apt-get install -y --no-install-recommends $RUN_DEPS \
     && rm -rf /var/lib/apt/lists/*
 
+RUN curl -sSL https://install.pi-hole.net | bash
+

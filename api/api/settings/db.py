93c54a198c650e142e1afff12d8b6c4f5d6a1c34
cclloyd <cclloyd@cclloyd.com>
2022-01-18 12:48:18 -0500

    Added other db support (beta).  Fixed migrations not applying

--- api/api/settings/db.py
+++ api/api/settings/db.py
@@ -6,21 +6,27 @@
 from . import env
 
 
-#DB_TYPE = 'django.db.backends.postgresql'
-DB_TYPE = 'django.db.backends.sqlite3'
+DB_TYPE = env.str('DB_TYPE', 'sqlite3')
 DB_HOST = env.str('DB_HOST', 'letsvpn')
 DB_PORT = env.int('DB_PORT', 5432)
 DB_NAME = env.str('DB_NAME', '/data/db.sqlite3')
 DB_USER = env.str('DB_USER', 'letsvpn')
 DB_PASS = env.str('DB_PASS', 'letsvpn')
 
-DATABASES = {
-    'default': {
-        'ENGINE': DB_TYPE,
-        'NAME': DB_NAME,
-        #'USER': DB_USER,
-        #'PASSWORD': DB_PASS,
-        #'HOST': DB_HOST,
-        #'PORT': DB_PORT
+if DB_HOST:
+    DATABASES = {
+        'default': {
+            'ENGINE': f'django.db.backends.{DB_TYPE}',
+            'USER': DB_USER,
+            'PASSWORD': DB_PASS,
+            'HOST': DB_HOST,
+            'PORT': DB_PORT
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': DB_TYPE,
+            'NAME': DB_NAME,
+        }
+    }
--- debian-root/etc/cont-init.d/30-migrate.sh
+++ debian-root/etc/cont-init.d/30-migrate.sh
@@ -2,4 +2,5 @@
 
 s6-echo "Running migrations..."
 exec sleep 5
-#exec python api/manage.py migrate || exit 1
+exec python api/manage.py migrate || exit 1
+
